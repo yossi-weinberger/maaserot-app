306fc9596b35d4c5fd07240609090ab87753a2af
Yossi Weinberger <137178593+yossi-weinberger@users.noreply.github.com>
2024-07-25 14:41:34 +0300

    EXE working

--- apps/desktop/main.js
+++ apps/desktop/main.js
@@ -1,10 +1,6 @@
-import { app, BrowserWindow } from "electron";
-import path from "path";
-import { fileURLToPath } from "url";
-import fs from "fs";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const fs = require("fs");
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -14,27 +10,26 @@ function createWindow() {
       nodeIntegration: false,
       contextIsolation: true,
       preload: path.join(__dirname, "preload.js"),
-      // webSecurity: false // זה מסוכן בייצור, אבל יעזור לנו לדבג
     },
   });
 
-  if (process.env.NODE_ENV === "development") {
-    win.loadURL("http://localhost:3000");
+  let indexPath;
+  if (app.isPackaged) {
+    indexPath = path.join(process.resourcesPath, "web", "out", "index.html");
   } else {
-    const indexPath = path.join(__dirname, "..", "web", "out", "index.html");
-    console.log("Trying to load:", indexPath);
-    if (fs.existsSync(indexPath)) {
-      win.loadFile(indexPath);
-    } else {
-      console.error("index.html not found at:", indexPath);
-      win.loadFile(path.join(__dirname, "error.html"));
-    }
+    indexPath = path.join(__dirname, "..", "web", "out", "index.html");
+  }
+
+  console.log("Trying to load:", indexPath);
+  if (fs.existsSync(indexPath)) {
+    win.loadFile(indexPath);
+  } else {
+    console.error("index.html not found at:", indexPath);
+    win.loadFile(path.join(__dirname, "error.html"));
   }
 
   win.webContents.openDevTools();
 }
-
-app.whenReady().then(createWindow);
 
 app.whenReady().then(createWindow);
 
@@ -49,3 +44,48 @@ app.on("activate", () => {
     createWindow();
   }
 });
+
+// const { app, BrowserWindow } = require("electron");
+// const path = require("path");
+// const fs = require("fs");
+
+// function createWindow() {
+//   const win = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nodeIntegration: false,
+//       contextIsolation: true,
+//       preload: path.join(__dirname, "preload.js"),
+//     },
+//   });
+
+//   if (process.env.NODE_ENV === "development") {
+//     win.loadURL("http://localhost:3000");
+//   } else {
+//     const indexPath = path.join(__dirname, "..", "web", "out", "index.html");
+//     console.log("Trying to load:", indexPath);
+//     if (fs.existsSync(indexPath)) {
+//       win.loadFile(indexPath);
+//     } else {
+//       console.error("index.html not found at:", indexPath);
+//       win.loadFile(path.join(__dirname, "error.html"));
+//     }
+//   }
+
+//   win.webContents.openDevTools();
+// }
+
+// app.whenReady().then(createWindow);
+
+// app.on("window-all-closed", () => {
+//   if (process.platform !== "darwin") {
+//     app.quit();
+//   }
+// });
+
+// app.on("activate", () => {
+//   if (BrowserWindow.getAllWindows().length === 0) {
+//     createWindow();
+//   }
+// });
